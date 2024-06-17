e0691369e6059f9ef290e7030ae087ca5b57a19d
Elcho911 <boss.armsport@gmail.com>
2024-06-18 05:23:16 +0600

    added refreshToken

--- dist/routes/index.js
+++ dist/routes/index.js
@@ -11,6 +11,13 @@ const corsConfig = {
     origin: [
         'http://localhost:3000',
         'http://localhost:5173',
+        'http://localhost:5174',
+        'http://localhost:5175',
+        'http://localhost:5176',
+        'http://localhost:5177',
+        'http://localhost:5178',
+        'http://localhost:5179',
+        'http://localhost:5180',
         'http://localhost:5000',
         'https://elchocrud.pro'
     ],
