0664eb772be6e001ae38a70b093f1be51b49bd72
Elcho911 <boss.armsport@gmail.com>
2024-06-18 18:43:06 +0600

    update todo

--- dist/constants.js
+++ dist/constants.js
@@ -1,12 +1,17 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.ACCESS_TOKEN_EXPIRATION = exports.COOKIE_SETTINGS = void 0;
-const isProduction = process.env.NODE_ENV !== 'development';
 exports.COOKIE_SETTINGS = {
-    REFRESH_TOKEN: {
-        httpOnly: true,
-        maxAge: 15 * 24 * 3600 * 1000,
-        secure: isProduction
-    }
+    REFRESH_TOKEN: process.env.NODE_ENV === 'development'
+        ? {
+            httpOnly: true,
+            maxAge: 15 * 24 * 3600 * 1000
+        }
+        : {
+            httpOnly: true,
+            maxAge: 15 * 24 * 3600 * 1000,
+            secure: true,
+            sameSite: 'None'
+        }
 };
 exports.ACCESS_TOKEN_EXPIRATION = 60 * 1000;
--- src/constants.ts
+++ src/constants.ts
@@ -1,11 +1,16 @@
-const isProduction = process.env.NODE_ENV !== 'development';
-
 export const COOKIE_SETTINGS: Record<string, Record<string, any>> = {
-	REFRESH_TOKEN: {
-		httpOnly: true,
-		maxAge: 15 * 24 * 3600 * 1000,
-		secure: isProduction
-	}
+	REFRESH_TOKEN:
+		process.env.NODE_ENV === 'development'
+			? {
+					httpOnly: true,
+					maxAge: 15 * 24 * 3600 * 1000
+				}
+			: {
+					httpOnly: true,
+					maxAge: 15 * 24 * 3600 * 1000,
+					secure: true,
+					sameSite: 'None'
+				}
 };
 
 export const ACCESS_TOKEN_EXPIRATION = 60 * 1000;
