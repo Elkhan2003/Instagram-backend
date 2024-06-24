08eb962aa8a6492dae4ed4f71096d794300b62fe
Elcho911 <boss.armsport@gmail.com>
2024-06-24 11:37:41 +0600

    maybe fix minor bug

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,20 +4,7 @@ import authRoutes from '../modules/auth/auth.routes';
 import chatsRoutes from '../modules/chats/chats.routes';
 
 const corsConfig = {
-	origin: [
-		'http://localhost:3000',
-		'http://localhost:5173',
-		'http://localhost:5174',
-		'http://localhost:5175',
-		'http://localhost:5176',
-		'http://localhost:5177',
-		'http://localhost:5178',
-		'http://localhost:5179',
-		'http://localhost:5180',
-		'http://localhost:5000',
-		'https://elchocrud.pro',
-		'https://peakspace.elcho.dev'
-	],
+	origin: '*',
 	credentials: true
 };
 
