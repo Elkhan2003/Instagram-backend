f473036899499d7f2dda7527bd0d9a9419cad945
Elcho911 <boss.armsport@gmail.com>
2024-06-25 08:18:38 +0600

    update

--- dist/constants.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.ACCESS_TOKEN_EXPIRATION = void 0;
-exports.ACCESS_TOKEN_EXPIRATION = new Date().getTime() + 15 * 60 * 1000;
--- src/constants.ts
+++
@@ -1,1 +0,0 @@
-export const ACCESS_TOKEN_EXPIRATION = new Date().getTime() + 15 * 60 * 1000;
