4c5c2dfb9cce6b9eb288e22a81528ec49de51dfd
Elcho911 <boss.armsport@gmail.com>
2024-06-29 10:36:59 +0600

    update upload file on supabase

--- dist/plugins/multer.js
+++ dist/plugins/multer.js
@@ -6,7 +6,21 @@ Object.defineProperty(exports, "__esModule", { value: true });
 exports.upload = void 0;
 const multer_1 = __importDefault(require("multer"));
 const storage = multer_1.default.memoryStorage();
-const types = ['image/png', 'image/jpeg', 'image/jpg'];
+const types = [
+    'image/png',
+    'image/jpeg',
+    'image/jpg',
+    'image/gif',
+    'image/webp',
+    'image/ico',
+    'image/svg+xml', // SVG-изображения
+    'image/bmp', // BMP-изображения
+    'image/tiff', // TIFF-изображения
+    'image/vnd.microsoft.icon', // ICO-изображения
+    'image/vnd.adobe.photoshop', // Photoshop PSD-файлы
+    'image/x-icon', // Ico-изображения
+    'image/vnd.dwg' // DWG-изображения
+];
 const fileFilter = (req, file, cb) => {
     if (types.includes(file.mimetype)) {
         cb(null, true);
--- src/plugins/multer.ts
+++ src/plugins/multer.ts
@@ -3,7 +3,21 @@ import { Request } from 'express';
 
 const storage: StorageEngine = multer.memoryStorage();
 
-const types = ['image/png', 'image/jpeg', 'image/jpg'];
+const types = [
+	'image/png',
+	'image/jpeg',
+	'image/jpg',
+	'image/gif',
+	'image/webp',
+	'image/ico',
+	'image/svg+xml', // SVG-изображения
+	'image/bmp', // BMP-изображения
+	'image/tiff', // TIFF-изображения
+	'image/vnd.microsoft.icon', // ICO-изображения
+	'image/vnd.adobe.photoshop', // Photoshop PSD-файлы
+	'image/x-icon', // Ico-изображения
+	'image/vnd.dwg' // DWG-изображения
+];
 
 const fileFilter = (
 	req: Request,
